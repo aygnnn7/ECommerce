d24e2b677a05e9358c878213c841a4ca0c08ed7d
Aygyun Ahmed <aygunahmed7@gmail.com>
2024-02-02 23:28:15 +0200

    Update: Add references based on Onion Architecture

---
+++ ECommerceAPI/Infrastructure/ECommerceAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            //services.AddSingleton<>();
+        }
+    }
+}
