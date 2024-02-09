b29b7781985686bb68fdf2820d8e72a6e407d26d
Aygyun Ahmed <aygunahmed7@gmail.com>
2024-02-09 20:41:42 +0200

    Add: Admin and user interface module infrastructure.

---
+++ ECommerceClient/src/app/admin/components/components.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomersModule } from './customers/customers.module';
+import { OrdersModule } from './orders/orders.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { ProductsModule } from './products/products.module';
+
+
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    CustomersModule,
+    OrdersModule,
+    DashboardModule,
+    ProductsModule
+  ],
+  exports: [
+  ]
+})
+export class ComponentsModule { }
---
+++ ECommerceClient/src/app/admin/layout/layout.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout.component';
+import { ComponentsModule } from './components/components.module';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    LayoutComponent
+  ],
+  imports: [
+    CommonModule,
+    ComponentsModule
+  ],
+  exports: [
+    LayoutComponent
+  ]
+})
+export class LayoutModule { }
