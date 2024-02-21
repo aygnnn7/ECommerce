453ddd88fa3bbcd6b65e53ff513546d40120a4e0
aygnnn7 <aygunahmed7@gmail.com>
2024-02-21 20:19:07 +0200

    Update: FileService: Renaming files during save operation in a way suitable for SEO

---
+++ ECommerceAPI/Infrastructure/ECommerceAPI.Infrastructure/Operations/NameOperation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Infrastructure.Operations
+{
+    public class NameOperation
+    {
+        public static string CharacterRegulatory(string name)
+        {
+            try
+            {
+                return name.Replace("/", "")
+                .Replace("\\", "")
+                .Replace("!", "")
+                .Replace("\"", "")
+                .Replace("£", "")
+                .Replace("$", "")
+                .Replace("%", "")
+                .Replace("^", "")
+                .Replace("&", "")
+                .Replace("*", "")
+                .Replace("(", "")
+                .Replace(")", "")
+                .Replace("+", "")
+                .Replace("}", "")
+                .Replace("{", "")
+                .Replace("[", "")
+                .Replace("]", "")
+                .Replace("#", "")
+                .Replace("'", "")
+                .Replace(";", "")
+                .Replace(",", "")
+                .Replace("_", "")
+                .Replace("<", "")
+                .Replace(">", "")
+                .Replace(".", "_");
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+              
+        }
+           
+        
+    }
+}
--- ECommerceAPI/Infrastructure/ECommerceAPI.Infrastructure/Services/FileService.cs
+++ ECommerceAPI/Infrastructure/ECommerceAPI.Infrastructure/Services/FileService.cs
@@ -1,4 +1,5 @@
 ﻿using ECommerceAPI.Application.Services;
+using ECommerceAPI.Infrastructure.Operations;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using System;
@@ -23,7 +24,7 @@ namespace ECommerceAPI.Infrastructure.Services
             {
                 await using FileStream fileStream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, useAsync: false);
 
-                await fileStream.CopyToAsync(fileStream);
+                await file.CopyToAsync(fileStream);
                 await fileStream.FlushAsync();
                 return true;
             }
@@ -34,9 +35,49 @@ namespace ECommerceAPI.Infrastructure.Services
             }
         }
 
-        public async Task<string> FileRenameAsync(string fileName)
+        private async Task<string> FileRenameAsync(string path, string fileName, bool first = true)
         {
-            throw new NotImplementedException();
+            string newFileName = await Task.Run(async () =>
+            {
+                string extension = Path.GetExtension(fileName);
+                string newFileName;
+                if (first)
+                {
+                    string oldName = Path.GetFileNameWithoutExtension(fileName);
+                    newFileName = $"{NameOperation.CharacterRegulatory(oldName)}{extension}";
+                }
+                else
+                {
+                    newFileName = fileName;
+                    int indexNo1 = newFileName.IndexOf('-');
+                    if(indexNo1 == -1)
+                    {
+                        newFileName = $"{Path.GetFileNameWithoutExtension(newFileName)}-2{extension}";
+                    }
+                    else
+                    {
+                        int indexNo2 = newFileName.IndexOf(".");
+                        int fileNumberStartIndex = indexNo1 + 1;
+                        int fileNumberLength = indexNo2 - indexNo1 - 1;
+
+                        string fileNo = newFileName.Substring(fileNumberStartIndex, fileNumberLength);
+                        int _fileNo = int.Parse(fileNo);
+                        _fileNo++;
+
+                        newFileName = newFileName.Remove(fileNumberStartIndex, fileNumberLength)
+                                                .Insert(fileNumberStartIndex, _fileNo.ToString());
+                    }
+                }
+
+                
+
+                if (File.Exists($"{path}\\{newFileName}"))
+                    return await FileRenameAsync(path, newFileName, false);
+                else
+                    return newFileName;
+            });
+
+            return newFileName;
         }
 
         public async Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection files)
@@ -49,7 +90,7 @@ namespace ECommerceAPI.Infrastructure.Services
             List<bool> results = new();
             foreach(IFormFile file in files)
             {
-                string fileNewName = await FileRenameAsync(file.FileName);
+                string fileNewName = await FileRenameAsync(uploadPath, file.FileName);
 
                 bool result = await CopyFileAsync($"{uploadPath}\\{fileNewName}", file);
                 datas.Add((fileNewName, $"{uploadPath}\\{fileNewName}"));
